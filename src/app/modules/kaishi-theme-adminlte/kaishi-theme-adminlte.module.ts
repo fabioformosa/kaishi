ea15dc5790821519aade348b6071862c509da143
Fabio Formosa <fabio.formosa@dedagroup.it>
2018-03-15 17:09:36 +0100

    <>

--- src/app/modules/kaishi-theme-adminlte/kaishi-theme-adminlte.module.ts
+++ src/app/modules/kaishi-theme-adminlte/kaishi-theme-adminlte.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { KaishiThemeAdminlteComponent } from './kaishi-theme-adminlte.component';
 
-import {NgxAdminLteModule, LayoutAuthComponent} from 'ngx-admin-lte';
+import {NgxAdminLteModule} from 'ngx-admin-lte';
 
 import {
     User,
@@ -21,7 +21,7 @@ import {
   declarations: [KaishiThemeAdminlteComponent],
   exports: [
     KaishiThemeAdminlteComponent,
-    LayoutAuthComponent
+    User
   ],
   providers : [
    MenuService,
