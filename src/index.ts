8d16aaa5d917fd5d67870d9ade895248d6c87de0
Fabio Formosa <fabio.formosa@dedagroup.it>
2018-03-16 10:42:18 +0100

    exported main module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { KaishiThemeAdminlteComponent } from './kaishi-theme-adminlte.component';
 export * from './kaishi-theme-adminlte.component';
+export * from './kaishi-theme-adminlte.module';
+
 
 import {
   NgxAdminLteModule, LayoutAuthComponent, User, MenuService, LogoService, FooterService
