add673e965ecf9735f794adb2daf70d0c1df49a7
Fabio Formosa <fabio.formosa@dedagroup.it>
2018-03-19 17:34:20 +0100

    fixed exports in public_api and other settings in angular-cli.json

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,6 @@
+import {
+  NgxAdminLteModule, LayoutAuthComponent, User, MenuService, LogoService, FooterService
+} from 'ngx-admin-lte';
+export * from 'ngx-admin-lte';
+
 export * from './src/app/modules/kaishi-theme-adminlte/kaishi-theme-adminlte.module'
